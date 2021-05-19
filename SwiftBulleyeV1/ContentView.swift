1e5aa94e82d9136c1077a3c6e6e17b5bdce106cf
arkdusdyk <arkdusdyk@gmail.com>
2021-05-20 05:30:51 +0900

    Make finalApp 7

--- SwiftBulleyeV1/ContentView.swift
+++ SwiftBulleyeV1/ContentView.swift
@@ -29,7 +29,8 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             VStack{
-                Spacer()
+                //Spacer()
+                Spacer().navigationBarTitle(" ◎ Bullseye ◎ ")
                 HStack {
                     Text("Put the bullseye as close as you can do:")
                         .modifier(LabelStyle())
@@ -44,6 +45,7 @@ struct ContentView: View {
                         .modifier(LabelStyle())
                     Slider(value: $sliderValue, in: 1...100)
                         .accentColor(Color.green)
+                        .animation(.easeOut)
                     Text("100")
                         .modifier(LabelStyle())
                 }
